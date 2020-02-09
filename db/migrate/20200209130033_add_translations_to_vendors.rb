52186228d03dbf1a9d460d9cc731558eca957b3d
kininwong <kininwong@gmail.com>
2020-02-09 21:07:07 +0800

    vendor globalize

---
+++ db/migrate/20200209130033_add_translations_to_vendors.rb
@@ -0,0 +1,12 @@
+class AddTranslationsToVendors < ActiveRecord::Migration[6.0]
+	def up
+		unless table_exists?(:spree_vendor_translations)
+		params = { name: :string , about_us: :text, contact_us: :text, address: :string, email: :string, speciality: :string }
+		Spree::Vendor.create_translation_table!(params, { migrate_data: true })
+		end
+	end
+
+	def down
+		Spree::Vendor.drop_translation_table! migrate_data: true
+	end
+end
